f84d1143955782a48dbaa88633e6c6f7fe912e89
Demon-Cat <870603664@qq.com>
2018-10-12 11:22:05 +0800

    单例模式

---
+++ main.cpp
@@ -0,0 +1,42 @@
+﻿#include "MainWindow.h"
+#include <QApplication>
+#include <QSharedMemory>
+#include <QMessageBox>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    QSharedMemory *shareMem = new QSharedMemory(QString("SingleInstanceIdentify"));
+
+    /* if the sharedmemory has not been created, it returns false, otherwise true.
+         * But if the application exit unexpectedly, the sharedmemory will not detach.
+         * So, we try twice.
+         */
+    volatile short i = 2;
+    while (i--)
+    {
+        if (shareMem->attach(QSharedMemory::ReadOnly)) /* no need to lock, bcs it's read only */
+        {
+            shareMem->detach();
+        }
+    }
+
+    int result = 0;
+    if (shareMem->create(1))
+    {
+        MainWindow w;
+        w.show();
+        result = a.exec();
+
+        if (shareMem->isAttached())
+            shareMem->detach();
+        delete shareMem;
+    }
+    else
+    {
+        QMessageBox::information(nullptr, "提示", "程序已经在运行。");
+    }
+
+    return result;
+}
